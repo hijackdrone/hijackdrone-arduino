34d585db3757ffac6f40774683b2cba3fa99acde
lsb918 <lsb918@uos.ac.kr>
2019-06-06 22:26:52 +0900

    serial test with python

--- arduino_rccar_sketch/arduino_rccar_sketch.ino
+++ arduino_rccar_sketch/arduino_rccar_sketch.ino
@@ -1,68 +1,72 @@
-#include <ArduinoJson.h>
+#include <AFMotor.h> // 모터드라이버L293D용 라이브러리
 
-const int capacity = JSON_OBJECT_SIZE(3) + 3*JSON_OBJECT_SIZE(1);
-StaticJsonDocument<capacity> doc;
+//Made by 상민
+
+AF_DCMotor motor1(1); // 전방 좌측 바퀴 M1 ==전방 좌측 바퀴는 M1에 연결 할 것!
+AF_DCMotor motor2(2); // 전방 우측 바퀴 M2 ==전방 우측 바퀴는 M2 "
+AF_DCMotor motor3(3); // 후방 좌측 바퀴 M3 ==후방 좌측 바퀴는 M3 "
+AF_DCMotor motor4(4); // 후방 우측 바퀴 M4 ==후방 우측 바퀴는 M4 "
 
 int led=13;
-int i=0;
-int tx=0;
-int rx=1;
-char data[200]={};
-int incomingByte=0;
+//시리얼 모니터를 통한 4방향 조정
+
+//"APP과의 연결만 끝나면 모든 개발 완료"
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(led,OUTPUT);
+  
+  motor1.setSpeed(200); // 모터 스피드(1~255)
+  motor2.setSpeed(200);
+  motor3.setSpeed(200);
+  motor4.setSpeed(200);
+  
+  motor1.run(RELEASE); // 처음에는 멈춘 상태
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+
 }
-char init(char arr[],int len){
-  int i;
-  for(i=0;i<len;i++){
-    arr[i]=0;
-  }
-}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available() > 0){
-    incomingByte = Serial.read();
-    if(incomingByte==13){
-      data[i++]='\0';
-      Serial.println("fin");
-      i=0;
-      DeserializationError err = deserializeJson(doc, data);
-      if(err){
-        Serial.print("err");
+  
+  if(Serial.available())
+  {   
+    char Dir = Serial.read();
+//    Serial.write(Dir);
+    Serial.println(Dir,DEC);
+    switch(Dir){
+      case 102: //f
+        motor1. run(BACKWARD);
+        motor2. run(FORWARD);
+        motor3. run(BACKWARD);
+        motor4. run(FORWARD);
         digitalWrite(led,HIGH);
-        delay(1000);
+        delay(100);
         digitalWrite(led,LOW);
-      }else{
-        char move=doc["to"];
-        switch(move){
-          case 'f': //front
-            digitalWrite(led,HIGH);
-            digitalWrite(rx,HIGH);
-            break;
-          case 'l': //left
-            digitalWrite(led,HIGH);
-            break;
-          case 'r': //right
-            digitalWrite(led,HIGH);
-            break;
-          case 'b': //back
-            digitalWrite(led,LOW);
-            break;
-          default: //stop
-            digitalWrite(led,HIGH);
-            delay(100);
-            digitalWrite(led,LOW);
-            break;
-        }
-      }
-      init(data,i);
-    }
-    else{
-      data[i++]=incomingByte;
-    }
-    Serial.print(incomingByte);
-    Serial.print(' ');
-  }
+        break;  
+      case 98: //b
+        digitalWrite(led,HIGH);
+        delay(100);
+        digitalWrite(led,LOW);
+        break;  
+      case 108: //l
+        digitalWrite(led,HIGH);
+        delay(100);
+        digitalWrite(led,LOW);
+        break;  
+      case 114: //r
+        digitalWrite(led,HIGH);
+        delay(100);
+        digitalWrite(led,LOW);
+        break;  
+      case 112: //p
+        digitalWrite(led,HIGH);
+        delay(100);
+        digitalWrite(led,LOW);
+        break;
+      default:
+        break;
+    } 
+  }   
 }
